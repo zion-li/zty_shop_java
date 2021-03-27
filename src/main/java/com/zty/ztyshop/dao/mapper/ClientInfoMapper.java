be8d0984af1429fba0dee0287cebfbb0e04d0cb3
zion-li <5275602+zion-li@user.noreply.gitee.com>
2021-03-27 10:20:03 +0800

    mapper

---
+++ src/main/java/com/zty/ztyshop/dao/mapper/ClientInfoMapper.java
@@ -0,0 +1,16 @@
+package com.zty.ztyshop.dao.mapper;
+
+import com.zty.ztyshop.dao.entity.ClientInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 客户表 Mapper 接口
+ * </p>
+ *
+ * @author 李佳 zion
+ * @since 2021-03-27
+ */
+public interface ClientInfoMapper extends BaseMapper<ClientInfo> {
+
+}
---
+++ src/main/java/com/zty/ztyshop/dao/mapper/StaffLevelMapper.java
@@ -0,0 +1,16 @@
+package com.zty.ztyshop.dao.mapper;
+
+import com.zty.ztyshop.dao.entity.StaffLevel;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 李佳 zion
+ * @since 2021-03-27
+ */
+public interface StaffLevelMapper extends BaseMapper<StaffLevel> {
+
+}
