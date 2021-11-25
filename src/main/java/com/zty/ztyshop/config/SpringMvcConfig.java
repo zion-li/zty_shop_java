0208167ae02cba707f27358145557b1079ee147b
lijia <lijia@autoai.com>
2021-11-25 11:04:27 +0800

    拦截器路径设置

--- src/main/java/com/zty/ztyshop/config/SpringMvcConfig.java
+++ src/main/java/com/zty/ztyshop/config/SpringMvcConfig.java
@@ -4,13 +4,10 @@ import com.zty.ztyshop.handler.LoginHandlerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
